c7bf6d17c65e55f8d1bc4c0beae2f525be10016b
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-09 19:05:49 +0100

    Fix Type error on incoming req

--- server/middleware/cloudflarePages.ts
+++ server/middleware/cloudflarePages.ts
@@ -1,7 +1,7 @@
 import type {IncomingMessage} from "unenv/runtime/node/http";
 
 export default defineEventHandler(async (event) => {
-  const req: IncomingMessage = event.node.req;
+  const req = event.node.req as IncomingMessage;
   const hostname = (req.headers.host ?? "").split(":")[0];
   if (hostname.endsWith(".pages.dev")) {
     setResponseHeader(event, "X-Robots-Tag", "noindex");
