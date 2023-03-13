e311affd4df7cf92a84ddd473f2a48a4b6643333
Lorenzo Lapucci <halexercraft@gmail.com>
2023-03-13 01:56:13 +0100

    Finish Login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="flex flex-col gap-18 items-start">
-    Login
-    <input type="text" ref="email" class="shadow-lg p-8"/>
-    <br>
-    <input type="password" ref="password" class="shadow-lg p-8"/>
+  <div class="flex flex-col justify-center items-center m-32">
+    <div class="bg-gray-100 drop-shadow-xl p-32 rounded flex flex-col gap-y-16">
+      <p class="text-h2">Login</p>
+      <div>
+        <p class="font-light">Email</p>
+        <input type="email" class="rounded w-full p-4" placeholder="info@pippo.com" v-model="email"/>
+      </div>
 
-    <br>
-    <br>
-    <button @click="login" class="border-2 p-8">Login</button>
-    <br>
-    <button @click="logout" class="border-2 p-8">LOGOUT</button>
+      <div>
+        <p class="font-light">Password</p>
+        <input type="password" class="rounded w-full p-4" placeholder="Password" v-model="password"/>
+      </div>
 
-    <pre class="m-4 p-8 bg-gray-200">{{ tokenStore.token }}</pre>
-    <pre class="m-4 p-8 bg-gray-200">{{ tokenStore.refreshToken }}</pre>
-    <pre class="m-4 p-8 bg-gray-200">{{ tokenStore.accountId }}</pre>
-    <p v-if="tokenStore.token">Sei loggato</p>
-    <p v-else>NON sei loggato</p>
+      <Button color="green" @click="login" :disabled="loading" class="cursor-pointer">
+        <div>Accedi</div>
+      </Button>
+
+      <div class="mt-32 text-small">
+        Non hai un account?
+        <nuxt-link to="/register" class="underline underline-offset-2">Registrati</nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +29,13 @@ import {UnauthorizedError} from "~/api/base";
 import {useTokenStore} from "~/stores/useTokenStore";
 
 export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      loading: false
+    }
+  },
   setup() {
     const tokenStore = useTokenStore()
 
@@ -33,27 +45,36 @@ export default {
   },
   methods: {
     async login() {
-      const email = this.$refs.email.value;
-      const password = this.$refs.password.value;
-      const response = await useApi().rawRequest("/v1/public/login", {
-        method: "POST",
-        body: JSON.stringify({email, password}),
-      }, false);
-      if (response.status !== 200) {
-        alert("Login failed");
-        return;
-      }
-      const token = response.headers.get("X-Auth-Token");
-      const refreshToken = response.headers.get("X-Auth-Refresh-Token");
+      this.loading = true;
+      const email = this.email;
+      const password = this.password;
+      try {
+        const response = await useApi().rawRequest("/v1/public/login", {
+          method: "POST",
+          body: JSON.stringify({email, password}),
+        }, false);
+        this.loading = false;
 
-      if (token) {
-        const [_, payload] = token.split(".");
-        const payloadDecoded = atob(payload);
-        const payloadJson = JSON.parse(payloadDecoded);
-        this.tokenStore.setToken(token);
-        this.tokenStore.setRefreshToken(refreshToken);
-        this.tokenStore.setRefreshTokenId(payloadJson.refreshTokenId);
-        this.tokenStore.setAccountId(payloadJson._id);
+        if (response.status !== 200) {
+          alert("Login failed");
+          return;
+        }
+        const token = response.headers.get("X-Auth-Token");
+        const refreshToken = response.headers.get("X-Auth-Refresh-Token");
+
+        if (token) {
+          const [_, payload] = token.split(".");
+          const payloadDecoded = atob(payload);
+          const payloadJson = JSON.parse(payloadDecoded);
+          this.tokenStore.setToken(token);
+          this.tokenStore.setRefreshToken(refreshToken);
+          this.tokenStore.setRefreshTokenId(payloadJson.refreshTokenId);
+          this.tokenStore.setAccountId(payloadJson._id);
+          this.$router.push("/");
+        }
+      } catch (e) {
+        this.loading = false;
+        alert("Login failed, server/network error");
       }
     },
     logout() {
